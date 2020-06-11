6315ce665606a61c2e20bf901eed6b070dd97d54
Noel <noel.cheng@rutgers.edu>
2020-06-11 15:42:59 -0400

    Update RegisterServlet.java

--- UserAccount/src/com/example/useraccount/controller/RegisterServlet.java
+++ UserAccount/src/com/example/useraccount/controller/RegisterServlet.java
@@ -1,9 +1,7 @@
 package com.example.useraccount.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
